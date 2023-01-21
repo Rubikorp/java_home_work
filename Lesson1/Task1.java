a2ba2ba67a2cc576c6fe178f546fbd2b18ade5fc
Ruslan Bikmetov <bik.big102rus@gmail.com>
2023-01-21 16:53:04 +0500

    refactoring task 1

--- Lesson1/Task1.java
+++ Lesson1/Task1.java
@@ -4,11 +4,16 @@
  */
 public class Task1 {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 2;
         System.out.println(factor(n));
+        System.out.println(sum(n));
     }
     static double factor(int n) {
         if(n==1)return 1;
         return n * factor(n-1);
+    }
+    static int sum(int n) {
+        if(n==1) return 1;
+        return n + sum(n-1);
     }
 }
