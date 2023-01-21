cceadf7f40d52a676804986128b44ecf4f6aeaa4
Ruslan Bikmetov <bik.big102rus@gmail.com>
2023-01-21 16:20:27 +0500

    add task 2

--- Lesson1/Task1.java
+++ Lesson1/Task1.java
@@ -1,7 +1,14 @@
-package Lesson1;
-
+/*
+ * Вычислить сумму чисел от 1 до n, 
+ * вычислить n! произведение чисел от 1 до n
+ */
 public class Task1 {
     public static void main(String[] args) {
-        
+        int n = 5;
+        System.out.println(factor(n));
+    }
+    static double factor(int n) {
+        if(n==1)return 1;
+        return n * factor(n-1);
     }
 }
